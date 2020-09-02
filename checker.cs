c928bbc8e813a8bfe9c659547f65ca5599d16730
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-09-02 22:45:14 +0530

    Adds method

--- checker.cs
+++ checker.cs
@@ -5,27 +5,27 @@ class Checker
 {
     static bool vitalsAreOk(float bpm, float spo2, float respRate)
         {
-            bool _vitalsVariable = false
-           
-            if(checkBPM(bpm) && checkSPO2(spo2) && checkRespRate(respRate)){
-                
-                _vitalsVariable = true;
+            if( checkSPO2(spo2) && checkBpmAndrespRate(bpm,respRate)){
+                return true;
             }
-            return _vitalsVariable;
+            return false;
+        }
+    
+        static bool checkBpmAndrespRate(float bpm, float respRate){
+            if(checkBPM(bpm) && checkRespRate(respRate)){
+                return true;
+            }
+            return false
         }
         
         static bool checkBPM(float bpm){
-            
             if (bpm < 70 || bpm > 150)
             {
                 return false;
             }
-            
             return true;
-            
         }
         static bool checkSPO2(float spo2){
-            
             if (spo2 < 90)
             {
                 return false;
@@ -34,14 +34,11 @@ class Checker
         }
         
         static bool checkRespRate(float respRate){
-            
             if (respRate < 30 || respRate > 95)
             {
                 return false;
             }
-            
             return true;
-            
         }
    
     static void ExpectTrue(bool expression) {
