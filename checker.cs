4a8fe765302d475425d5256391f3bfe68a72a743
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-09-03 22:56:01 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -1,15 +1,62 @@
 using System;
 using System.Diagnostics;
 
-namespace namespaceChecker{
-
-    class Checker
-    {
-        static int Main() {
-            VitalsChecker.ExpectTrue(VitalsChecker.vitalsAreOk(100, 95, 60));
-            VitalsChecker.ExpectFalse(VitalsChecker.vitalsAreOk(40, 91, 92));
-            Console.WriteLine("All ok");
-            return 0;
+class Checker
+{
+    static bool vitalsAreOk(float bpm, float spo2, float respRate)
+        {
+            if(checkSPO2(spo2) && checkBpmAndrespRate(bpm,respRate)){
+                return true;
+            }
+            return false;
         }
+    
+        static bool checkBpmAndrespRate(float bpm, float respRate){
+            if(checkBPM(bpm) && checkRespRate(respRate)){
+                return true;
+            }
+            return false;
+        }
+        
+        static bool checkBPM(float bpm){
+            if (bpm < 70 || bpm > 150)
+            {
+                return false;
+            }
+            return true;
+        }
+        static bool checkSPO2(float spo2){
+            if (spo2 < 90)
+            {
+                return false;
+            }
+            return true;
+        }
+        
+        static bool checkRespRate(float respRate){
+            if (respRate < 30 || respRate > 95)
+            {
+                return false;
+            }
+            return true;
+        }
+   
+    static void ExpectTrue(bool expression) {
+        if(!expression) {
+            Console.WriteLine("Expected true, but got false");
+            Environment.Exit(1);
+        }
+    }
+    static void ExpectFalse(bool expression) {
+        if(expression) {
+            Console.WriteLine("Expected false, but got true");
+            Environment.Exit(1);
+        }
+    }
+    static int Main() {
+        ExpectTrue(vitalsAreOk(100, 95, 60));
+        ExpectFalse(vitalsAreOk(40, 91, 92));
+        Console.WriteLine("All ok");
+        return 0;
     }
 }
