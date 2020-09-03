b1baa1e938c7c47a60b7d9c82cbd1222ddd5fa80
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-09-03 22:46:31 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Diagnostics;
 
-class Checker
-{
-    static int Main() {
-        VitalsChecker.ExpectTrue(VitalsChecker.vitalsAreOk(100, 95, 60));
-        VitalsChecker.ExpectFalse(VitalsChecker.vitalsAreOk(40, 91, 92));
-        Console.WriteLine("All ok");
-        return 0;
+namespace namespaceChecker{
+
+    class Checker
+    {
+        static int Main() {
+            VitalsChecker.ExpectTrue(VitalsChecker.vitalsAreOk(100, 95, 60));
+            VitalsChecker.ExpectFalse(VitalsChecker.vitalsAreOk(40, 91, 92));
+            Console.WriteLine("All ok");
+            return 0;
+        }
     }
 }
